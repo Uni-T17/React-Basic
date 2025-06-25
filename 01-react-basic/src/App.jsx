374759167f5e83ed6c198f1d968f6d21a1e36d9b
UniT-17 <mintayzamt17@gmail.com>
2025-06-25 23:47:52 +0700

    Learn Set State

--- 01-react-basic/src/App.jsx
+++ 01-react-basic/src/App.jsx
@@ -3,18 +3,19 @@ import "./App.css";
 import TapButton from "./components/TapButton";
 
 function App() {
-  const [fruits, setFruit] = useState(["Apple", "Orange"]);
+  const [user, setUser] = useState({ name: "Kit", age: 12, isSingle: false });
   return (
     <>
       <div>
-        <ul>
-          {fruits.map((fruit) => (
-            <li>{fruit}</li>
-          ))}
-        </ul>
+        <p>My name is {user.name}</p>
+        <p>I am {user.age} years old.</p>
+        {user.isSingle ? <p>I am a Single.</p> : <p>I am in a relationship.</p>}
+
         <TapButton
-          buttonName={"Add Fruit"}
-          onClick={() => setFruit((prev) => [...prev, "Apple"])}
+          buttonName={"Click"}
+          onClick={() =>
+            setUser((prev) => ({ ...prev, isSingle: !user.isSingle }))
+          }
         />
       </div>
     </>
