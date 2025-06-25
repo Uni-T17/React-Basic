c43dc594e8ce1f048214db3104a1f74490396d4a
UniT-17 <mintayzamt17@gmail.com>
2025-06-25 23:57:50 +0700

    Learn set state with object

--- 01-react-basic/src/App.jsx
+++ 01-react-basic/src/App.jsx
@@ -3,13 +3,29 @@ import "./App.css";
 import TapButton from "./components/TapButton";
 
 function App() {
-  const [user, setUser] = useState({ name: "Kit", age: 12, isSingle: false });
+  const [user, setUser] = useState({
+    name: "",
+    age: 12,
+    isSingle: false,
+  });
   return (
     <>
       <div>
         <p>My name is {user.name}</p>
         <p>I am {user.age} years old.</p>
         {user.isSingle ? <p>I am a Single.</p> : <p>I am in a relationship.</p>}
+
+        <form>
+          <label htmlFor="myName"></label>
+          <input
+            type="text"
+            id="myName"
+            value={user.name}
+            onChange={(e) =>
+              setUser((prev) => ({ ...prev, name: e.target.value }))
+            }
+          />
+        </form>
 
         <TapButton
           buttonName={"Click"}
